b578f512afeaf2d3fdb40289bb3da85e2ccf44c6
muhd-raf1 <muhd.raaffi@gmail.com>
2024-12-11 01:47:51 +0800

    membuat logic repository untuk mencari semua buku yang categorinya sama, findBooksSameGenre()

--- src/main/java/toko/buku/toko_buku/Repository/Category/CategoryRepositoryImpl.java
+++ src/main/java/toko/buku/toko_buku/Repository/Category/CategoryRepositoryImpl.java
@@ -1,8 +1,15 @@
 package toko.buku.toko_buku.Repository.Category;
 
+import java.util.List;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
 import toko.buku.Utilities.UtilEntityManagerFactoy;
 import toko.buku.toko_buku.Entity.CategoriesEntity;
 
@@ -69,7 +76,34 @@ public class CategoryRepositoryImpl implements CategoriRepository {
     }
 
     @Override
-    public void findBooksSameGenre() {
+    public void findBooksSameGenre(CategoriesEntity categoriesEntity) {
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+            CriteriaQuery<CategoriesEntity> query = criteriaBuilder.createQuery(CategoriesEntity.class);
+            Root<CategoriesEntity> c = query.from(CategoriesEntity.class);
+            c.fetch("books", JoinType.LEFT);
+
+            query.select(c);
+
+            query.where(
+                    criteriaBuilder.equal(c.get("id"), categoriesEntity.getId()));
+
+            TypedQuery<CategoriesEntity> typedQuery = entityManager.createQuery(query);
+            List<CategoriesEntity> resultList = typedQuery.getResultList();
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            transaction.rollback();
+        }
     }
 
 }

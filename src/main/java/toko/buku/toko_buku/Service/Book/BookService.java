fff0eff83be81ca51896ee41cbcb3c8b9693aef6
muhd-raf1 <muhd.raaffi@gmail.com>
2024-12-14 16:06:07 +0800

    mengubah data type return untk method add dari void ke boolean

--- src/main/java/toko/buku/toko_buku/Service/Book/BookService.java
+++ src/main/java/toko/buku/toko_buku/Service/Book/BookService.java
@@ -4,7 +4,7 @@ import toko.buku.toko_buku.Entity.BooksEntity;
 
 public interface BookService {
 
-    public void add(BooksEntity book);
+    public boolean add(BooksEntity book);
 
     public void delete(String id);
 

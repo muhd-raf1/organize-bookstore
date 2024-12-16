68652c01c2189209c004cf098bfb1e12032463a9
muhd-raf1 <muhd.raaffi@gmail.com>
2024-12-17 07:51:43 +0800

    membuat testing untuk mengetes method findBookssamGenre di class Service, di segala kondisi

--- src/test/java/toko/buku/toko_buku/Service/CategoryTest.java
+++ src/test/java/toko/buku/toko_buku/Service/CategoryTest.java
@@ -1,8 +1,11 @@
 package toko.buku.toko_buku.Service;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import toko.buku.toko_buku.Entity.BooksEntity;
 import toko.buku.toko_buku.Entity.CategoriesEntity;
 import toko.buku.toko_buku.Repository.Category.CategoriRepository;
 import toko.buku.toko_buku.Repository.Category.CategoryRepositoryImpl;
@@ -48,5 +51,41 @@ public class CategoryTest {
     void testAddNullIdFail() {
         boolean b = categoryService.add(new CategoriesEntity(null, "null"));
         Assertions.assertFalse(b);
+    }
+
+    @Test
+    void testFindBooksSameGenreSuccess() {
+        List<CategoriesEntity> test = categoryService.findBooksSameCategory("Fantasy");
+        Assertions.assertNotNull(test);
+
+        for (CategoriesEntity testing : test) {
+            for (BooksEntity testBook : testing.getBooks()) {
+                System.out.println(testBook.getTitle());
+            }
+        }
+    }
+
+    @Test
+    void testFindBookSameGenreNullFail() {
+        List<CategoriesEntity> booksSameCategory = categoryService
+                .findBooksSameCategory(null);
+
+        Assertions.assertNull(booksSameCategory);
+    }
+
+    @Test
+    void testFindBookSameGenreEmptyStringFail() {
+        List<CategoriesEntity> booksSameCategory = categoryService
+                .findBooksSameCategory("   ");
+
+        Assertions.assertNull(booksSameCategory);
+    }
+
+    @Test
+    void testFindBookSameGenreNotFoundFail() {
+        List<CategoriesEntity> booksSameCategory = categoryService
+                .findBooksSameCategory("tod");
+
+        Assertions.assertNull(booksSameCategory);
     }
 }

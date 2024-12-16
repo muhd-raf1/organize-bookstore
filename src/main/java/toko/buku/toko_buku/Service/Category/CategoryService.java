dbe0d63be790807eb709485c31ff41779396ee5f
muhd-raf1 <muhd.raaffi@gmail.com>
2024-12-17 07:49:17 +0800

    mengubah data type return-nya method findBooksSameGenre dari void ke List<CategoriesEntity>

--- src/main/java/toko/buku/toko_buku/Service/Category/CategoryService.java
+++ src/main/java/toko/buku/toko_buku/Service/Category/CategoryService.java
@@ -1,11 +1,13 @@
 package toko.buku.toko_buku.Service.Category;
 
+import java.util.List;
+
 import toko.buku.toko_buku.Entity.CategoriesEntity;
 
 public interface CategoryService {
 
     public boolean add(CategoriesEntity category);
 
-    public void findBooksSameCategory();
+    public List<CategoriesEntity> findBooksSameCategory(String categoryName);
 
 }
